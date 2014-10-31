77ca67ae48aa6515cce8cd9f1d8d9b0b1a83ab3d
Matteo Latini <mtylty@gmail.com>
2014-10-31 16:52:24 +0100

    adds pry prompt configuration

--- lib/dev_tools/railtie.rb
+++ lib/dev_tools/railtie.rb
@@ -15,10 +15,19 @@ AwesomePrint.pry!
 
 module DevTools
   class Railtie < Rails::Railtie
-    config.before_initialize do
-      Rails.application.configure do
-        config.action_mailer.delivery_method = :letter_opener if Rails.env.development?
-      end
+    config.action_mailer.delivery_method = :letter_opener if Rails.env.development?
+
+    initializer 'dev_tools.initialize' do |app|
+      Pry.config.prompt_name = "\001\e[1;32m\002#{Rails.application.class.parent_name.downcase}\001\e[0m\002/\001\e[1;31m\002#{Rails.env.downcase}"
+
+      Pry.prompt = [
+        proc { |target_self, nest_level, pry|
+          "[#{pry.input_array.size}] #{Pry.config.prompt_name} \001\e[0m\002(\001\e[0;33m\002#{Pry.view_clip(target_self)}\001\e[0m\002) #{":#{nest_level}" unless nest_level.zero?}> "
+        },
+        proc { |target_self, nest_level, pry|
+          "[#{pry.input_array.size}] #{Pry.config.prompt_name} \001\e[0m\002(\001\e[0;33m\002#{Pry.view_clip(target_self)}\001\e[0m\002) #{":#{nest_level}" unless nest_level.zero?}< "
+        }
+      ]
     end
   end
 end
