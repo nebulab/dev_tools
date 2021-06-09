0ce2a7f0154cf947580bc19f96071a64e83dba6f
vassalloandrea <andrea.vassallo.94@gmail.com>
2021-06-09 19:15:39 +0200

    Use the new Pry APIs
    Fix the Pry prompt deprecation warning
    using the new APIs.
    
    https://github.com/pry/pry/issues/1836

--- lib/dev_tools/railtie.rb
+++ lib/dev_tools/railtie.rb
@@ -4,13 +4,8 @@ require 'pry-doc'
 require 'pry-byebug'
 require 'awesome_print'
 
-if Rails.env.development?
-  require 'letter_opener'
-end
-
-if Rails.env.development? || Rails.env.test?
-  require 'dotenv-rails'
-end
+require 'letter_opener' if Rails.env.development?
+require 'dotenv-rails' if Rails.env.development? || Rails.env.test?
 
 AwesomePrint.pry!
 
@@ -18,7 +13,7 @@ module DevTools
   class Railtie < Rails::Railtie
     config.action_mailer.delivery_method = :letter_opener if Rails.env.development?
 
-    initializer 'dev_tools.initialize' do |app|
+    initializer 'dev_tools.initialize' do
       Pry.config.should_load_rc = false
 
       parent_name = if Rails::VERSION::MAJOR >= 6
@@ -27,16 +22,15 @@ module DevTools
                       Rails.application.class.parent_name
                     end
 
-      Pry.config.prompt_name = "\001\e[1;32m\002#{parent_name.downcase}\001\e[0m\002/\001\e[1;31m\002#{Rails.env.downcase}"
-
-      Pry.prompt = [
-        proc { |target_self, nest_level, pry|
-          "[#{pry.input_array.size}] #{Pry.config.prompt_name} \001\e[0m\002(\001\e[0;33m\002#{Pry.view_clip(target_self)}\001\e[0m\002) #{":#{nest_level}" unless nest_level.zero?}> "
-        },
-        proc { |target_self, nest_level, pry|
-          "[#{pry.input_array.size}] #{Pry.config.prompt_name} \001\e[0m\002(\001\e[0;33m\002#{Pry.view_clip(target_self)}\001\e[0m\002) #{":#{nest_level}" unless nest_level.zero?}< "
-        }
-      ]
+      Pry::Prompt.add(
+        "\001\e[1;32m\002#{parent_name.downcase}\001\e[0m\002/\001\e[1;31m\002#{Rails.env.downcase}",
+        'Includes the current Rails environment.',
+        %w[> <]
+      ) do |target_self, nest_level, pry, sep|
+        "[#{pry.input_array.size}] " \
+        "#{Pry.config.prompt_name} \001\e[0m\002(\001\e[0;33m\002#{Pry.view_clip(target_self)}" \
+        "\001\e[0m\002) #{":#{nest_level}" unless nest_level.zero?}#{sep} "
+      end
     end
   end
 end
